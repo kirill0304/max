986cb0842dbad02a8500227071f221eb30f562d9
kirill0304 <03041999kirill@gmail.com>
2018-01-21 21:58:59 +0200

    init

---
+++ 1.py
@@ -0,0 +1,11 @@
+ 
+
+# max = []
+# for i in range(10000):
+#     max.append("I LOVE SASHA")    
+# print("\n".join(max))
+
+# for i in range(int(input())):
+#     print("I LOVE SASHA")
+print (colored('{0:{width}}'.format('Hello World!', width = 79), 'green','on_white'))
+print (colored('{0:{width}}'.format('Hi!', width = 79), 'green', 'on_white'))       
